7b236f374f8a13624b1d0a0d7248a46347f4ad27
ycdfwzy <ycdfwzy@outlook.com>
2018-10-16 14:49:59 +0800

    static debug

--- adminpage/views.py
+++ adminpage/views.py
@@ -10,8 +10,10 @@ from wechat.views import CustomWeChatView
 
 # Create your views here.
 
+
 def getCurrentTime():
-    return int( datetime(datetime.now()).timestamp() )
+    return int( datetime.now().timestamp() )
+
 
 def get_index(id, buttons):
     book_header = CustomWeChatView.event_keys['book_header']
@@ -19,6 +21,7 @@ def get_index(id, buttons):
         if buttons['key'] == book_header+str(id):
             return i+1
     return 0
+
 
 class loginPage(APIView):
 
@@ -46,14 +49,14 @@ class activityList(APIView):
         ret = []
         for act in actived_activities:
             re = {}
-            re['id'] = act['id']
-            re['name'] = act['name']
-            re['description'] = act['description']
-            re['startTime'] = act['start_time']
-            re['endTime'] = act['end_time']
-            re['place'] = act['place']
-            re['bookStart'] = act['book_start']
-            re['bookEnd'] = act['book_end']
+            re['id'] = act.id
+            re['name'] = act.name
+            re['description'] = act.description
+            re['startTime'] = act.start_time
+            re['endTime'] = act.end_time
+            re['place'] = act.place
+            re['bookStart'] = act.book_start
+            re['bookEnd'] = act.book_end
             re['currentTime'] = getCurrentTime()
             if act['status'] == Activity.STATUS_PUBLISHED:
                 re['status'] = 1
@@ -94,26 +97,26 @@ class activityDetail(APIView):
         except:
             raise LogicError("get activity by id")
         ret = {}
-        ret['name'] = res['name']
-        ret['key'] = res['key']
-        ret['description'] = res['description']
-        ret['startTime'] = res['start_time']
-        ret['endTime'] = res['end_time']
-        ret['place'] = res['place']
-        ret['bookStart'] = res['book_start']
-        ret['bookEnd'] = res['book_end']
-        ret['totalTickets'] = res['total_tickets']
-        ret['picUrl'] = res['pic_url']
-        ret['bookedTickets'] = res['total_tickets']-res['remain_tickets']
+        ret['name'] = res.name
+        ret['key'] = res.key
+        ret['description'] = res.description
+        ret['startTime'] = res.start_time
+        ret['endTime'] = res.end_time
+        ret['place'] = res.place
+        ret['bookStart'] = res.book_start
+        ret['bookEnd'] = res.book_end
+        ret['totalTickets'] = res.total_tickets
+        ret['picUrl'] = res.pic_url
+        ret['bookedTickets'] = res.total_tickets-res.remain_tickets
         ret['currentTime'] = getCurrentTime()
-        if res['status'] == Activity.STATUS_PUBLISHED:
+        if res.status == Activity.STATUS_PUBLISHED:
             ret['status'] = 1
         else: ret['status'] = 0
 
         cnt = 0
         ress = Ticket.get_by_activity(res)
         for res in ress:
-            if res['status'] == Ticket.STATUS_USED:
+            if res.status == Ticket.STATUS_USED:
                 cnt = cnt+1
         ret['usedTickets'] = cnt
 
@@ -131,41 +134,41 @@ class activityDetail(APIView):
         except:
             raise LogicError("get activity by id error!")
 
-        if res['status'] != Activity.STATUS_PUBLISHED:
-            if res['name'] != self.input['name']:
+        if res.status != Activity.STATUS_PUBLISHED:
+            if res.name != self.input['name']:
                 raise LogicError('can\'t  modify name')
-            if res['place'] != self.input['place']:
+            if res.place != self.input['place']:
                 raise LogicError('can\'t  modify place')
-            if res['book_start'] != self.input['bookStart']:
+            if res.book_start != self.input['bookStart']:
                 raise LogicError('can\'t  modify book_start')
-            if res['status'] != self.input['status'] and self.input['status'] == 0:
+            if res.status != self.input['status'] and self.input['status'] == 0:
                 raise LogicError('can\'t  modify status')
 
         curTime = getCurrentTime()
-        if curTime > res['start_time']:
-            if res['book_end'] != self.input['bookEnd']:
+        if curTime > res.start_time:
+            if res.book_end != self.input['bookEnd']:
                 raise LogicError('can\'t  modify book_end')
 
-        if curTime > res['end_time']:
-            if res['start_time'] != self.input['startTime']:
+        if curTime > res.end_time:
+            if res.start_time != self.input['startTime']:
                 raise LogicError('can\'t  modify start_time')
-            if res['end_time'] != self.input['endTime']:
+            if res.end_time != self.input['endTime']:
                 raise LogicError('can\'t  modify end_time')
 
         if curTime > res['totalTickets']:
-            if res['total_tickets'] != self.input['totalTickets']:
+            if res.total_tickets != self.input['totalTickets']:
                 raise LogicError('can\'t  modify total_tickets')
 
-        res['name'] = self.input['name']
-        res['place'] = self.input['place']
-        res['description'] = self.input['description']
-        res['pic_url'] = self.input['picUrl']
-        res['start_time'] = self.input['startTime']
-        res['end_time'] = self.input['endTime']
-        res['book_start'] = self.input['bookStart']
-        res['book_end'] = self.input['bookEnd']
-        res['total_tickets'] = self.input['totalTickets']
-        res['status'] = self.input['status']
+        res.name = self.input['name']
+        res.place = self.input['place']
+        res.description = self.input['description']
+        res.pic_url = self.input['picUrl']
+        res.start_time = self.input['startTime']
+        res.end_time = self.input['endTime']
+        res.book_start = self.input['bookStart']
+        res.book_end = self.input['bookEnd']
+        res.total_tickets = self.input['totalTickets']
+        res.status = self.input['status']
         res.save()
         return
 
@@ -188,9 +191,9 @@ class activityMenu(APIView):
         ret = []
         for re in res:
             RET = {}
-            RET['id'] = re['id']
-            RET['name'] = re['name']
-            RET['menuIndex'] = get_index(re['id'], buttons)
+            RET['id'] = re.id
+            RET['name'] = re.name
+            RET['menuIndex'] = get_index(re.id, buttons)
             ret.append(RET)
         return ret
 
