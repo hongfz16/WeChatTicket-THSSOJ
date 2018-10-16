b9317ddf509c246a4bc4f92ba46b531444a3a263
hongfz <hongfz16@163.com>
2018-10-17 01:26:01 +0800

    fix a full night of bugs

--- adminpage/views.py
+++ adminpage/views.py
@@ -54,9 +54,9 @@ class logoutPage(APIView):
     def post(self):
         print("logoutPage post")
         # print(self.request.user)
-        if not self.request.user.is_authenticated():
-            raise LogicError('logout error!')
         auth.logout(self.request)
+        if self.request.user.is_authenticated():
+            raise LogicError('logout error!')
 
 class activityList(APIView):
 
