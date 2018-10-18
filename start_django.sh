52a43a4327d7f4786a0834b8dcb271d0795ddfa2
hongfz <hongfz16@163.com>
2018-10-18 15:56:29 +0800

    add three web server

--- start_django.sh
+++ start_django.sh
@@ -1,4 +1,5 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${ADMIN}', '${ADMIN_EMAIL}', '${ADMIN_PASSWORD}')" | python manage.py shell
-python3 manage.py runserver 0.0.0.0:8000
+#python3 manage.py runserver 0.0.0.0:8000
+uwsgi --http :${MPORT} --module WeChatTicket.wsgi
